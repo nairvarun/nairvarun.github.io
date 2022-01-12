da0d7643843d41a4b6b063f747e023dfc2e22f6f
varun <10447555-nairvarun@users.noreply.gitlab.com>
2022-01-12 21:48:42 +0530

    p9-like site: functional

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('👀')
+// TODO: add more p9 like features
+    // text selection stuff
+    // custom context menu
+
+
+// TODO: add custom dialog box for this
+document.getElementById('email-redirect').addEventListener('click', () => {
+    if (window.confirm("open mail?")) {
+        window.location.href = "mailto:nairvarun@pm.me";
+    }
+});
