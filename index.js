6e4416a367d4f2681d23ec4af2ecc1beb0ecfa50
varun <10447555-nairvarun@users.noreply.gitlab.com>
2022-01-15 01:14:49 +0530

    refactored

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+console.log(
+    "%cheloo", 
+    "font-size:25px;"
+);
+    
 // confirm before opening mail because opening directly is annoying
 document.querySelector('#email-redirect').addEventListener('click', () => {
     if (window.confirm('open mail?')) {
@@ -5,7 +10,6 @@ document.querySelector('#email-redirect').addEventListener('click', () => {
     }
 });
 
-// redirect to nerd mode
 document.querySelector('#heading__box').addEventListener('click', () => {
-    window.location.href = './src/html/nerd-mode.html';
+    window.location.href = './src/html/plumber.html';
 });
